d80f277a892647c60293d42438796f693bac364f
fausto08 <faustol99@gmail.com>
2023-04-04 20:33:12 -0400

    displays hard coded info to html page

--- the_weather/weather/views.py
+++ the_weather/weather/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+import requests
 
-# Create your views here.
+
+def index(request):
+    API = 'bec728d68fa54dd3a813df9e47d550a3'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units=imperial'
+    city = 'New york'
+
+    res = requests.get(url.format(city, API)).json()
+
+    city_weather = {
+        'city': city,
+        'temperature': res['main']['temp'],
+        'description': res['weather'][0]['description'],
+        'icon': res['weather'][0]['icon'],
+    }
+    context = {'city_weather': city_weather}
+
+    return render(request, 'weather/index.html', context)
